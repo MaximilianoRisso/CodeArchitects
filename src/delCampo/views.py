ea296c14066f2ff091895ec36482f0245bac7bff
MaximilianoRisso <3993MaxiRisso@users.noreply.github.com>
2020-02-17 16:48:16 -0300

    first commit

---
+++ src/delCampo/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home_page(request):
+    return render(request, "home.html", {"title": "Home"})
+
+def login_page(request):
+    return render(request, "login.html", {"title": "Login"})
